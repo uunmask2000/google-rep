2ed9d04308c8852e16da3b249c1fef3c0d4a729b
jim <chuang.jim@baiyitech.com.tw>
2020-07-28 14:45:41 +0700

    ret

--- Api.php
+++ Api.php
@@ -13,41 +13,50 @@ class Google_recaptcha_v3
     public function get_Google_recaptcha_v3()
     {
 
-        $url = self::api_host;
-        $secret = self::secret;
+        echo    $this->captcha  ;
+        // $url = self::api_host;
+        // $secret = self::secret;
 
-        $url .= "?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR'];
+        // $url .= "?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR'];
 
-        $response = file_get_contents($url);
-        // use json_decode to extract json response
-        $response = json_decode($response);
+        // $response = file_get_contents($url);
+        // // use json_decode to extract json response
+        // $response = json_decode($response);
 
-        return $response;
+        // return $response;
 
     }
 }
 
-if (isset($_POST['g-recaptcha-response'])) {
-    $captcha = $_POST['g-recaptcha-response'];
-} else {
-    $captcha = false;
-}
-if (!$captcha) {
-    die('captcha error');
-} else {
-    $Google_recaptcha_v3 = new Google_Recaptcha($_POST);
-    $response = $Google_recaptcha_v3->get_Google_recaptcha_v3();
-    print_r($response);
-    if ($response->success === false) {
-        //Do something with error
-        die('response error');
-    }
+echo '<pre>';
+$params['g-recaptcha-response'] = '03AGdBq26gVGDA8X_xa-HegSHU3nlC7CUn3oB-SmaurfcbQuwlg7Tl8hzycQr0TgWDtmnyCo5_MAcZnZxvDDcO_khRJs6EbaVS_HZDXD2ufLpKFPrude6l7vVvcMeCzrQQNdGx3VB1veP6shaMoiwBjinKHyEG4_iUOWWyzdrHMptk63JBc6KvC7xeML5lSB0XNtrkk4zpeI9yCgpEWFE3AmrRXNfxCB88nRIvQNGdK_SQOKnMxOuVU--4Ot4Fm39b7evslgX53jJkydMRTFjRiAKluYEFDxES5Z8a5obeWtaogkVxEZHoCeeLZpVPp45zE1TCWXytCyr1-46ha84ir-AI8qBrTTpOUVmTu1ZGL7oEPgQl_YVoVSiNfQAP4bkP6uQgCbuuYND5_xQcB3eXPv3Ec8VOGshtwkciBVQFSwdimyTZws1cGek'; 
+print_r($params);
+$Google_recaptcha_v3 = new Google_Recaptcha($params);
+$response = $Google_recaptcha_v3->get_Google_recaptcha_v3();
+print_r($response);
 
-    if ($response->success == true && $response->score <= 0.5) {
-        //Do something to denied access
-        echo '通过验证';
-    } else {
-        echo '不通过验证';
-    }
 
-}
+// if (isset($_POST['g-recaptcha-response'])) {
+//     $captcha = $_POST['g-recaptcha-response'];
+// } else {
+//     $captcha = false;
+// }
+// if (!$captcha) {
+//     die('captcha error');
+// } else {
+//     $Google_recaptcha_v3 = new Google_Recaptcha($_POST);
+//     $response = $Google_recaptcha_v3->get_Google_recaptcha_v3();
+//     print_r($response);
+//     if ($response->success === false) {
+//         //Do something with error
+//         die('response error');
+//     }
+
+//     if ($response->success == true && $response->score <= 0.5) {
+//         //Do something to denied access
+//         echo '通过验证';
+//     } else {
+//         echo '不通过验证';
+//     }
+
+// }
