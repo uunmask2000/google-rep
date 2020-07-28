55da6065575e11035e6b63d59258e6c74cfa1571
jim <chuang.jim@baiyitech.com.tw>
2020-07-28 14:57:37 +0700

    try

--- Api.php
+++ Api.php
@@ -4,7 +4,7 @@ class Google_Recaptcha
 
     // V3
     const secret = '6LfrJLcZAAAAAJJ5a-YfsAg_arWfg0xcsBtQjlZV';
-    const api_host = 'https://www.google.com/recaptcha/api/siteverify?s';
+    const api_host = 'https://www.google.com/recaptcha/api/siteverify';
 
     public function _construct()
     {
@@ -17,7 +17,7 @@ class Google_Recaptcha
         $url = self::api_host;
         $secret = self::secret;
 
-        echo $url .= "?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR'];
+        $url .= "?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR'];
 
         $response = file_get_contents($url);
         // // use json_decode to extract json response
@@ -28,34 +28,18 @@ class Google_Recaptcha
     }
 }
 
-echo '<pre>';
 $captcha = '03AGdBq26gVGDA8X_xa-HegSHU3nlC7CUn3oB-SmaurfcbQuwlg7Tl8hzycQr0TgWDtmnyCo5_MAcZnZxvDDcO_khRJs6EbaVS_HZDXD2ufLpKFPrude6l7vVvcMeCzrQQNdGx3VB1veP6shaMoiwBjinKHyEG4_iUOWWyzdrHMptk63JBc6KvC7xeML5lSB0XNtrkk4zpeI9yCgpEWFE3AmrRXNfxCB88nRIvQNGdK_SQOKnMxOuVU--4Ot4Fm39b7evslgX53jJkydMRTFjRiAKluYEFDxES5Z8a5obeWtaogkVxEZHoCeeLZpVPp45zE1TCWXytCyr1-46ha84ir-AI8qBrTTpOUVmTu1ZGL7oEPgQl_YVoVSiNfQAP4bkP6uQgCbuuYND5_xQcB3eXPv3Ec8VOGshtwkciBVQFSwdimyTZws1cGek';
 
 $class = new Google_Recaptcha();
 $response = $class->result($captcha);
-print_r($response);
-
-// if (isset($_POST['g-recaptcha-response'])) {
-//     $captcha = $_POST['g-recaptcha-response'];
-// } else {
-//     $captcha = false;
-// }
-// if (!$captcha) {
-//     die('captcha error');
-// } else {
-//     $Google_recaptcha_v3 = new Google_Recaptcha($_POST);
-//     $response = $Google_recaptcha_v3->get_Google_recaptcha_v3();
-//     print_r($response);
-//     if ($response->success === false) {
-//         //Do something with error
-//         die('response error');
-//     }
-
-//     if ($response->success == true && $response->score <= 0.5) {
-//         //Do something to denied access
-//         echo '通过验证';
-//     } else {
-//         echo '不通过验证';
-//     }
-
-// }
+$res = false;
+if ($response->success === false) {
+    $res = false;
+} else if ($response->success == true && $response->score <= 0.5) {
+    //Do something to denied access
+    $res = true;
+} else {
+    $res = false;
+}
+echo $res;
+return;
