e00742b2842496dbca69de7c3b05e1eeb404709e
jim <chuang.jim@baiyitech.com.tw>
2020-07-28 15:07:56 +0700

    rey

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
 <script src="https://www.google.com/recaptcha/api.js?render=6LfrJLcZAAAAADXGDWfjsEO7HjnuabjT5eQzTVNs"></script>
 <script>
     grecaptcha.ready(function () {
