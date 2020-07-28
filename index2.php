5a7afafaf10edf13f903e6c352d83a5483eec497
jim <chuang.jim@baiyitech.com.tw>
2020-07-28 12:27:39 +0700

    trtt

--- index2.php
+++ index2.php
@@ -1,13 +1,15 @@
-<script src="https://www.google.com/recaptcha/api.js?render=6LfrJLcZAAAAAJJ5a-YfsAg_arWfg0xcsBtQjlZV"></script>
+<script src="https://www.google.com/recaptcha/api.js?render=6LfrJLcZAAAAADXGDWfjsEO7HjnuabjT5eQzTVNs"></script>
 <script>
-    grecaptcha.ready(function() {
-    // do request for recaptcha token
-    // response is promise with passed token
-        grecaptcha.execute('6LfrJLcZAAAAAJJ5a-YfsAg_arWfg0xcsBtQjlZV', {action:'validate_captcha'})
-                  .then(function(token) {
-            // add token value to form
-            document.getElementById('g-recaptcha-response').value = token;
-        });
+    grecaptcha.ready(function () {
+        // do request for recaptcha token
+        // response is promise with passed token
+        grecaptcha.execute('6LfrJLcZAAAAADXGDWfjsEO7HjnuabjT5eQzTVNs', {
+                action: 'validate_captcha'
+            })
+            .then(function (token) {
+                // add token value to form
+                document.getElementById('g-recaptcha-response').value = token;
+            });
     });
 </script>
 
@@ -15,5 +17,5 @@
 <form id="form_id" method="post" action="your_action.php">
     <input type="text" readonly="readonly" id="g-recaptcha-response" name="g-recaptcha-response">
     <input type="text" readonly="readonly" name="action" value="validate_captcha">
-     
+
 </form>
