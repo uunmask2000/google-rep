69e8bdac562695b27e6ad9309b3696c781aa88dd
jim <chuang.jim@baiyitech.com.tw>
2020-07-28 12:03:34 +0700

    try

--- index.php
+++ index.php
@@ -1,17 +1,53 @@
 <html>
   <head>
-    <title>reCAPTCHA demo: Simple page</title>
-     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-     <script>
-       function onSubmit(token) {
-         document.getElementById("demo-form").submit();
-       }
-     </script>
+    <title>reCAPTCHA demo: Explicit render for multiple widgets</title>
+    <script type="text/javascript">
+      var verifyCallback = function(response) {
+        alert(response);
+      };
+      var widgetId1;
+      var widgetId2;
+      var onloadCallback = function() {
+        // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
+        // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
+        widgetId1 = grecaptcha.render('example1', {
+          'sitekey' : '6Le4IbcZAAAAALzVIMD0Ufsjx6dXT39puBRhgPZf',
+          'theme' : 'light'
+        });
+        widgetId2 = grecaptcha.render(document.getElementById('example2'), {
+          'sitekey' : '6Le4IbcZAAAAALzVIMD0Ufsjx6dXT39puBRhgPZf'
+        });
+        grecaptcha.render('example3', {
+          'sitekey' : '6Le4IbcZAAAAALzVIMD0Ufsjx6dXT39puBRhgPZf',
+          'callback' : verifyCallback,
+          'theme' : 'dark'
+        });
+      };
+    </script>
   </head>
   <body>
-    <form id='demo-form' action="?" method="POST">
-      <button class="g-recaptcha" data-sitekey="6LehIbcZAAAAAJme43XgwXLscKcZGK5ORn0HTS0e" data-callback='onSubmit'>Submit</button>
-      <br/>
+    <!-- The g-recaptcha-response string displays in an alert message upon submit. -->
+    <form action="javascript:alert(grecaptcha.getResponse(widgetId1));">
+      <div id="example1"></div>
+      <br>
+      <input type="submit" value="getResponse">
     </form>
+    <br>
+    <!-- Resets reCAPTCHA widgetId2 upon submit. -->
+    <form action="javascript:grecaptcha.reset(widgetId2);">
+      <div id="example2"></div>
+      <br>
+      <input type="submit" value="reset">
+    </form>
+    <br>
+    <!-- POSTs back to the page's URL upon submit with a g-recaptcha-response POST parameter. -->
+    <form action="?" method="POST">
+      <div id="example3"></div>
+      <br>
+      <input type="submit" value="Submit">
+    </form>
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+        async defer>
+    </script>
   </body>
 </html>
